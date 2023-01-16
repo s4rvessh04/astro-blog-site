b219446a74a25ed21413253f7b9507be0f01f875
sarveshrane2000 <sarveshrane2000@gmail.com>
2023-01-16 12:00:06 +0530

    add: astro image and tailwindcss typography

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,11 +6,17 @@ module.exports = {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
-			body: ["Inter", "sans-serif", ...defaultTheme.fontFamily.sans],
-			sans: ["Inter", "sans-serif", ...defaultTheme.fontFamily.sans],
-			number: ["Space Grotesk", "sans-serif"],
+			// body: ["Inter", "sans-serif", ...defaultTheme.fontFamily.sans],
+			// sans: ["Inter", "sans-serif", ...defaultTheme.fontFamily.sans],
+			body: ["Mona Sans", "sans-serif", ...defaultTheme.fontFamily.sans],
+			sans: ["Mona Sans", "sans-serif", ...defaultTheme.fontFamily.sans],
+			number: [
+				"Hubot Sans",
+				"sans-serif",
+				...defaultTheme.fontFamily.mono,
+			],
 		},
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require("@tailwindcss/typography")],
 };
