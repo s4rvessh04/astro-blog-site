d5200c8eaccff9b3b4dcc32f19ed4702cc0a4048
s4rvessh04 <sarveshrane2000@gmail.com>
2023-09-25 18:36:39 +0530

    add: project deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,51 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-
-// https://astro.build/config
+import { defineConfig } from "astro/config";
+import fs from "fs";
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+import remarkUnwrapImages from "remark-unwrap-images";
+// @ts-ignore:next-line
+import { remarkReadingTime } from "./src/utils/remark-reading-time.mjs";
 
-// https://astro.build/config
-import image from "@astrojs/image";
+function rawFonts(ext) {
+	return {
+		name: "vite-plugin-raw-fonts",
+		// @ts-ignore:next-line
+		transform(_, id) {
+			if (ext.some((e) => id.endsWith(e))) {
+				const buffer = fs.readFileSync(id);
+				return {
+					code: `export default ${JSON.stringify(buffer)}`,
+					map: null,
+				};
+			}
+		},
+	};
+}
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), image()]
+	site: "https://blog-sarvessh.vercel.app/",
+	markdown: {
+		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
+		remarkRehype: { footnoteLabelProperties: { className: [""] } },
+		shikiConfig: {
+			theme: "dracula",
+			wrap: true,
+		},
+	},
+	integrations: [
+		mdx({}),
+		tailwind({
+			applyBaseStyles: false,
+		}),
+		sitemap(),
+		prefetch(),
+	],
+	vite: {
+		plugins: [rawFonts([".ttf"])],
+		optimizeDeps: {
+			exclude: ["@resvg/resvg-js"],
+		},
+	},
 });
